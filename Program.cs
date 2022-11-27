d3e3a90ede09cd1492707370ab6637b8bdc071af
AlexanderL22 <112512188+AlexanderL22@users.noreply.github.com>
2022-11-27 15:05:02 +0300

    Update Program.cs
    Final

--- Program.cs
+++ Program.cs
@@ -8,3 +8,21 @@ using System.Linq;
 Clear();
 
 WriteLine ("Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,длина которых меньше, либо равна 3 символам.");
+
+string[] M;
+WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+       
+    }
+}
+WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
